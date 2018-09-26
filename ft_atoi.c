8f3b3dfb07de3473e7e2b15f46af029285e4a89f
eua1024 <41024@i.ua>
2018-09-26 03:06:01 +0300

    Remove unnecessary iterator

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,29 +10,27 @@ static int ft_is_whitespace(char c)
 
 int ft_atoi(char *str)
 {
-	int i;
 	int sign;
 	unsigned long int result;
 
-	i = 0;
 	sign = 1;
 	result = 0;
-	while (ft_is_whitespace(str[i]))
-		i++;
-	if (str[i] == '-')
+	while (ft_is_whitespace(*str))
+		str++;
+	if (*str == '-')
 		sign = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while ((str[i] != '\0') && ft_isdigit(str[i]))
+	if (*str == '-' || *str == '+')
+		str++;
+	while ((*str != '\0') && ft_isdigit(*str))
 	{
 		if ((result > 922337203685477580 || (result == 922337203685477580
-			&& (str[i] - '0') > 7)) && sign == 1)
+			&& (*str - '0') > 7)) && sign == 1)
 			return (-1);
 		else if ((result > 922337203685477580 || (result == 922337203685477580
-			&& (str[i] - '0') > 8)) && sign == -1)
+			&& (*str - '0') > 8)) && sign == -1)
 			return (0);
-		result = (result * 10) + (str[i] - '0');
-		i++;
+		result = (result * 10) + (*str - '0');
+		str++;
 	}
 	return ((int)(result * sign));
 }
